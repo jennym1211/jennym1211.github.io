da810edff2c12e7b9478c3ab94a2646d519955de
Jennifer Mackey <jennym1211@hotmail.com>
2021-05-18 00:28:45 -0400

    Added some pages

---
+++ loadnavs.js
@@ -0,0 +1,12 @@
+document.getElementById("headerNav").innerHTML =
+  "<ul><li><a href='index.html'>Home</a></li>" +
+  "<li><a href='portfolio.html'>Portfolio</a></li>" +
+  "<li><a href='about.html'>About</a></li></ul></header>";
+
+document.getElementById("footerNav").innerHTML =
+  "<ul><li> <a href='https://github.com/jennym1211'>" +
+  " <span style='font-size: 3em; color: black'></span>" +
+  "<i class='fab fa-github fa-2x'></i></a></li>" +
+  "<li><a href='https://www.linkedin.com/in/jennym1211/'>" +
+  "<span style='font-size: 3em; color: black'></span>" +
+  "<i class='fab fa-linkedin fa-2x'></i></a></li></ul>";
