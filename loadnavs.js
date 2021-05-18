66f3f8d7947f1a8a7f60bcbd013d8e24c86d3a50
Jennifer Mackey <jennym1211@hotmail.com>
2021-05-18 00:58:36 -0400

    updates

--- loadnavs.js
+++ loadnavs.js
@@ -1,12 +1,12 @@
 document.getElementById("headerNav").innerHTML =
-  "<ul><li><a href='index.html'>Home</a></li>" +
+  "<nav><ul><li><a href='index.html'>Home</a></li>" +
   "<li><a href='portfolio.html'>Portfolio</a></li>" +
-  "<li><a href='about.html'>About</a></li></ul></header>";
+  "<li><a href='about.html'>About</a></li></ul></nav>";
 
 document.getElementById("footerNav").innerHTML =
-  "<ul><li> <a href='https://github.com/jennym1211'>" +
+  "<nav><ul><li> <a href='https://github.com/jennym1211' target='_blank' rel='noopener noreferrer'>" +
   " <span style='font-size: 3em; color: black'></span>" +
   "<i class='fab fa-github fa-2x'></i></a></li>" +
-  "<li><a href='https://www.linkedin.com/in/jennym1211/'>" +
+  "<li><a href='https://www.linkedin.com/in/jennym1211/' target='_blank' rel='noopener noreferrer'>" +
   "<span style='font-size: 3em; color: black'></span>" +
-  "<i class='fab fa-linkedin fa-2x'></i></a></li></ul>";
+  "<i class='fab fa-linkedin fa-2x'></i></a></li> </ul><div id='copyright'>Copyright  &#8471; 2021 Jennifer Mackey</div></nav>";
