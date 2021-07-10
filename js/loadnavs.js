0af67e1d6e36d5233684711b12e6314f664b113b
Jennifer Mackey <jennym1211@hotmail.com>
2021-07-10 14:57:31 -0500

    Some Updates

---
+++ js/loadnavs.js
@@ -0,0 +1,12 @@
+document.getElementById("headerNav").innerHTML =
+  "<nav><ul id='headerul'><li class='headerli'><a href='index.html'>Home</a></li>" +
+  "<li class='headerli'><a href='portfolio.html'>Portfolio</a></li>" +
+  "</ul></nav>";
+
+document.getElementById("footerNav").innerHTML =
+  "<nav><ul id='footerul'><li class='ftrli'> <a href='https://github.com/jennym1211' target='_blank' rel='noopener noreferrer'>" +
+  " <span style='font-size: 3em; color: black'></span>" +
+  "<i aria-hidden='true' class='fab fa-github fa-2x'></i></a></li>" +
+  "<li class='ftrli'> <a href='https://www.linkedin.com/in/jennym1211/' title='linkedin' target='_blank' rel='noopener noreferrer'>" +
+  "<span class='sr-only' style='font-size: 3em; color: black'>LinkedIn</span> " +
+  "<i class='fab fa-linkedin fa-2x'></i></a></li> </ul></nav><div id='copyright'>Copyright  &#8471; 2021 Jennifer Mackey</div>";
