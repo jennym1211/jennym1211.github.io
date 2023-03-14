16a5465d1dc59a376039bd7455a7e16fdee953e7
Jennifer Mackey <jennym1211@hotmail.com>
2023-03-14 04:41:04 -0500

    Updates

--- portfolio-app/src/components/ProjectsIndex.vue
+++ portfolio-app/src/components/ProjectsIndex.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import ermassage from "../assets/ermassage.png";
+import stimmy from "../assets/stimmyio.png";
 </script>
 <template>
   <div class="mx-20">
-    <h2 class="text-xl mb-2 bg-[#95a7b6] text-white">
-      Current Complete Projects
-    </h2>
+    <h2 class="text-xl mb-2 bg-[#95a7b6] text-white">Projects</h2>
 
-    <span class="text-sm"> Note: More to be added soon.</span>
+    <img :src="ermassage" class="h-100" />
 
-    <div class="mx-20">
-      <a href="https://www.ermassagetx.com/" target="_blank">
-        <img :src="ermassage" class="h-96"
-      /></a>
+    <div class="flex flex-row mx-10">
+      <span class="bg-[#8B5B6E] text-white my-4 mx-auto p-4">
+        A former small business massage establishment located in Texas. Made
+        with: HTML, CSS, Javascript
+      </span>
+    </div>
 
-      <div class="flex flex-row mx-10">
-        <span class="bg-[#8B5B6E] text-white m-4 p-4">
-          A small business massage establishment located in Texas. Made with:
-          HTML, CSS, Javascript</span
-        >
-      </div>
+    <img :src="stimmy" class="h-100" />
+
+    <div class="flex flex-row mx-10">
+      <span class="bg-[#8B5B6E] text-white m-4 p-4">
+        <b class="text-xl"> stimmy.io</b><br /><br />
+        (Coming Soon) A web application tailored and dedicated to neurodivergent
+        individuals to help manage their symptoms. (More layouts and info coming
+        soon)
+      </span>
     </div>
   </div>
 </template>
